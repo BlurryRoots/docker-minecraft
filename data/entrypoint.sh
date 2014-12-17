c9c7797d4fd1208e50fd551d2450e832c783f1f7
blurryroots <blurryroots@posteo.de>
2014-12-17 19:16:39 +0100

    folder is in tmp, duh

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -6,6 +6,6 @@
 # Unzip map and copy to data folder
 (test -d /data/WorldOfKrepel-Captive || \
 	echo "copying map data" && unzip -d /tmp /server/map.zip && \
-	mv "Captive Minecraft" /data/WorldOfKrepel-Captive) && \
+	mv "/tmp/Captive Minecraft" /data/WorldOfKrepel-Captive) && \
 	# And run the server
 	java -Xmx${srv_mem} -jar minecraft_server.jar nogui
