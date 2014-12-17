afc72a0eedb3b6c706002a883d761efa09412980
blurryroots <blurryroots@posteo.de>
2014-12-17 18:54:34 +0100

    debug message

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copy server into data folder if it is not already there
-test -f /data/minecraft_server.jar || cp /server/minecraft_server.jar /data
-# And run the server
-java -Xmx${srv_mem} -jar /data/minecraft_server.jar nogui
+(test -f /data/minecraft_server.jar || \
+	echo "copying server" && cp /server/minecraft_server.jar /data) && \
+	# And run the server
+	java -Xmx${srv_mem} -jar /data/minecraft_server.jar nogui
